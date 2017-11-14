d0dcff51b4f551f887dedb49b4512427789ce002
Bartnik Tomasz <Tomasz.Bartnik@poland.asseco.corp>
2017-11-14 13:15:39 +0100

    initial commit

---
+++ src/main/java/edu/pwr/LibraryApp/LibraryAppRunner.java
@@ -0,0 +1,12 @@
+package edu.pwr.LibraryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAppRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAppRunner.class, args);
+	}
+}
